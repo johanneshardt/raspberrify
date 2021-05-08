d851deda3b8d588776a487dee08f394f978ed754
johanneshardt <46093733+johanneshardt@users.noreply.github.com>
2021-05-09 01:10:47 +0200

    Fixed type hint

--- raspberrify/sense.py
+++ raspberrify/sense.py
@@ -1,10 +1,11 @@
 from sense_hat import SenseHat
+from typing import List
 
 sense = SenseHat()
 sense.set_rotation(r=180)
 
 
-def show(matrix: list[list[int]]) -> None:
+def show(matrix: List[List[int]]) -> None:
     if len(matrix) == 64 and all(len(e) == 3 for e in matrix):
         sense.set_pixels(matrix)
     else:
