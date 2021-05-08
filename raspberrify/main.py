46a8053d0234484db969b1b9d162eb6f9c9dff6c
johanneshardt <46093733+johanneshardt@users.noreply.github.com>
2021-05-08 14:36:32 +0200

    Initial credential handling

--- raspberrify/main.py
+++ raspberrify/main.py
@@ -1,21 +1,13 @@
-from PIL import Image
 import pprint
 import requests
 import raspberrify.spotify as spotify
+from dotenv import dotenv_values
 
 def main() -> None:
-    sp = spotify.authorize()
-    track = sp.currently_playing()
-    if track is not None:
-        name = track['item']['name']
-        image_url = track['item']['album']['images'][0]['url']
 
-        im = Image.open(requests.get(image_url, stream=True).raw)
-        pprint.pprint(name)
-        im.show()
-    else:
-        print("No track is currently playing.")
-
+    config = dotenv_values("../.env.secrets")
+    for key, value in config.items():
+        print(key, value)
 
 if __name__ == "__main__":
     main()
